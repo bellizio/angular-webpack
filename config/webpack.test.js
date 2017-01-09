d63f718632db7d0c3938e99cb66e6418c8bf806c
Devon Bellizio <jdbellizio@gmail.com>
2017-01-09 09:55:15 -0500

    remove empty string extension; add -loader suffix

--- config/webpack.test.js
+++ config/webpack.test.js
@@ -4,27 +4,30 @@ module.exports = {
   devtool: 'inline-source-map',
 
   resolve: {
-    extensions: ['', '.ts', '.js']
+    extensions: ['.ts', '.js']
   },
 
   module: {
     loaders: [
       {
         test: /\.ts$/,
-        loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+        loaders: [
+          'awesome-typescript-loader',
+          'angular2-template-loader'
+        ]
       },
       {
         test: /\.html$/,
-        loader: 'html'
+        loader: 'html-loader'
       },
       {
         test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-        loader: 'null'
+        loader: 'null-loader'
       },
       {
         test: /\.scss$/,
         include: helpers.root('src', 'assets', 'css'),
-        loader: 'null'
+        loader: 'null-loader'
       }
     ]
   }
